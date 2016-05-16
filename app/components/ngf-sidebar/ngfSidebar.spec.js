4011e7d599db2c7dcb3186b8ae4e7cb4fdd01169
dtomaszewski.js@gmail.com <dtomaszewski.js@gmail.com>
2016-05-16 10:00:04 +0200

    ngf sidebar tests

--- app/components/ngf-sidebar/ngfSidebar.spec.js
+++ app/components/ngf-sidebar/ngfSidebar.spec.js
@@ -1,5 +1,43 @@
-describe('Sidebar', () => {
-    it('', () => {
+describe('Directive : ngfSidebar', () => {
+    let scope;
+    let element;
 
+    beforeEach(module('templates'));
+    beforeEach(module('ngf-sidebar'));
+
+    beforeEach(inject(($rootScope, $compile) => {
+        scope = $rootScope.$new();
+
+        scope.links = [
+            {
+                sref: 'main',
+                title: 'Stocks page'
+            },
+            {
+                sref: 'currency',
+                title: 'Currency page'
+            }
+        ];
+
+        element = angular.element('<ngf-sidebar links="links"></ngf-sidebar>');
+
+        element = $compile(element)(scope);
+        $rootScope.$digest();
+    }));
+
+    it('should have proper values in isolated scope', () => {
+        const isolatedScope = element.isolateScope();
+
+        expect(isolatedScope.links).toEqual(scope.links);
+    });
+
+    it('should have listed all passed links in template', () => {
+        const sections = element.find('section');
+        expect(sections.length).toBe(2);
+
+        expect(sections.eq(0).find('md-button').attr('ui-sref')).toBe('main');
+        expect(sections.eq(0).find('md-button').text()).toBe('Stocks page');
+        expect(sections.eq(1).find('md-button').attr('ui-sref')).toBe('currency');
+        expect(sections.eq(1).find('md-button').text()).toBe('Currency page');
     });
 });
