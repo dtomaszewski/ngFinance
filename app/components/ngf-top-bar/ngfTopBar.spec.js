85370490e05c61459a19ba7f1111ca34885d51e8
dtomaszewski.js@gmail.com <dtomaszewski.js@gmail.com>
2016-05-16 10:04:19 +0200

    ngf top bar tests

--- app/components/ngf-top-bar/ngfTopBar.spec.js
+++ app/components/ngf-top-bar/ngfTopBar.spec.js
@@ -1,5 +1,28 @@
-describe('Top bar', () => {
-    it('', () => {
-        
-    }); 
+describe('Directive : ngfTopBar', () => {
+    let scope;
+    let element;
+
+    beforeEach(module('templates'));
+    beforeEach(module('ngf-top-bar'));
+
+    beforeEach(inject(($rootScope, $compile) => {
+        scope = $rootScope.$new();
+
+        scope.title = 'Top bar test title';
+
+        element = angular.element('<ngf-top-bar title="{{ title }}"></ngf-top-bar>');
+
+        element = $compile(element)(scope);
+        $rootScope.$digest();
+    }));
+
+    it('should have proper values in isolated scope', () => {
+        const isolatedScope = element.isolateScope();
+
+        expect(isolatedScope.title).toEqual('Top bar test title');
+    });
+
+    it('should show title in template', () => {
+        expect(element.find('h1').text()).toBe('Top bar test title');
+    });
 });
