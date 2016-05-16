dacc5c11cb012e892e68621352aac7ff46663673
dtomaszewski.js@gmail.com <dtomaszewski.js@gmail.com>
2016-05-16 12:44:23 +0200

    Currency service

--- app/services/currency/Currency.js
+++ app/services/currency/Currency.js
@@ -3,8 +3,7 @@
 angular.module('currency', []).factory('Currency', ($http, $q, $httpParamSerializer) => {
     const url = 'https://community-neutrino-currency-conversion.p.mashape.com/convert';
     const headers = {
-        // 'X-Mashape-Key': '40sdHLz5OdmshKi9i7UcEGu2vIyip1on4mbjsn74nDQ3BQgo3S',
-        'X-Mashape-Key': 'pJbeIVwdEtEzDt8xrDVy2dPF8MTFFuT0KiZ97bgGyaIK6I56',
+        'X-Mashape-Key': '40sdHLz5OdmshKi9i7UcEGu2vIyip1on4mbjsn74nDQ3BQgo3S',
         'Content-Type': 'application/x-www-form-urlencoded'
     };
 
